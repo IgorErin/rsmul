225d78e4e8cb533a954f435d98bac7902fd48903
IgorErin <erin.igore@yandex.ru>
2024-04-24 12:33:33 +0300

    refactor: path fix

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use clap::Parser;
 use sprs::io::{read_matrix_market, write_matrix_market};
+
+use std::env;
 
 #[derive(Parser, Debug)]
 struct Cli {
     #[arg(short, long, default_value_t = 1u32)]
     count: u32,
+}
 
-    #[arg(short, long)]
-    src_path: std::path::PathBuf,
+fn string_of_pathbuf(pathbuf: PathBuf) -> String {
+    pathbuf
+        .into_os_string()
+        .into_string()
+        .expect("buf to string conversion failed")
+}
 
-    #[arg(short, long)]
-    dest_path: std::path::PathBuf,
+fn get_current_dir() -> PathBuf {
+    env::current_dir().expect("unable to take current dir")
 }
 
 fn mk_result_path(src: &PathBuf, count: u32, dest_dir: &PathBuf) -> PathBuf {
-    let name_os = src.file_name().expect("file name doesnt exist");
-    let name_str = name_os.to_os_string().into_string().unwrap();
+    let name = take_name(src);
+    let result_name = format!("{}{}", name, count + 1);
 
-    let mut result = dest_dir
-        .clone()
-        .into_os_string()
-        .into_string()
-        .expect("buf to string conversiont failed");
+    let mut result = dest_dir.clone();
+    result.push(result_name);
 
-    result.push_str(&name_str);
-    result.push_str(&format!("{count}"));
-
-    PathBuf::from(result)
+    result
 }
 
 fn run_path(src_path: PathBuf, count: u32, dest_path: &PathBuf) {
@@ -42,32 +43,36 @@ fn run_path(src_path: PathBuf, count: u32, dest_path: &PathBuf) {
     result.unwrap_or_else(|e| panic!("writing error: {e}"));
 }
 
+fn mk_src_glob() -> String {
+    let mut s = get_current_dir();
+    s.push("*.mtx");
+
+    string_of_pathbuf(s)
+}
+
+fn take_name(path: &Path) -> String {
+    let name_os = path.file_name().expect("file name doesnt exist");
+
+    name_os.to_os_string().into_string().unwrap()
+}
+
+fn check_glob(glob : Result<PathBuf, glob::GlobError>) -> PathBuf {
+    glob.unwrap_or_else(|x| panic!("glob error: {x}"))
+}
+
 fn main() {
     let args = Cli::parse();
 
-    let src_glog = {
-        let mut s = args
-            .src_path
-            .into_os_string()
-            .into_string()
-            .expect("Path to string convertation failed");
-
-        s.push_str("/*.mtx");
-
-        s
-    };
-
-    println!("src glob: {:?}", src_glog);
+    let src_glog = mk_src_glob();
+    println!("src glob: {:?}\n", src_glog);
 
     for src_path in glob::glob(&src_glog).expect("glob erorr") {
-        println!("src path: {:?}", src_path);
+        let src_path = check_glob(src_path);
+        println!("start: {:?}", take_name(&src_path));
 
-        let src_path = src_path.expect("glob error...");
-
-        let dest_path = mk_result_path(&src_path, args.count, &args.dest_path);
-
-        println!("dest path: {:?}", &dest_path);
-
+        let dest_path = mk_result_path(&src_path, args.count, &get_current_dir());
         run_path(src_path, args.count, &dest_path);
+
+        println!("end with: {:?}", take_name(&dest_path));
     }
 }
